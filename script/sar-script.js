790c4b7e17e69ce9584cc147ad4fc3072781d6de
SardineFish <Sardine@live.cn>
2017-12-11 00:34:57 +0800

    Complete backgound mask lineal transform

--- script/sar-script.js
+++ script/sar-script.js
@@ -7,27 +7,86 @@ function load()
 
     canvas.height = height;
     canvas.width = width;
+    var rectWrapHeight = 220;
     var rectData = [
         {
-            height: 200,
-            wrapHeight:220,
             width: 500,
+            height: 200,
             x: 100,
             pos: { x: 0, y: 0 }
         },
         {
-            height: 200,
-            wrapHeight: 220,
             width: 600,
+            height: 200,
             x: -100,
             pos: { x: 0, y: 0 }
         },
         {
-            height: 200,
-            wrapHeight: 220,
             width: 600,
+            height: 200,
             x: 150,
             pos: { x: 0, y: 0 }
+        }
+    ];
+    var rectTransform = [
+        {
+            width: 500,
+            height: 200,
+            x: 100,
+            pos: { x: 0, y: 0 }
+        },
+        {
+            width: 600,
+            height: 200,
+            x: -100,
+            pos: { x: 0, y: 0 }
+        },
+        {
+            width: 600,
+            height: 200,
+            x: 150,
+            pos: { x: 0, y: 0 }
+        }
+    ];
+    var targetData = rectData;
+    var nextRect = [
+        {
+            width: 400,
+            height: 200,
+            x: -100,
+            pos: { x: 0, y: 0 }
+        },
+        {
+            width: 500,
+            height: 200,
+            x: 0,
+            pos: { x: 0, y: 0 }
+        },
+        {
+            width: 600,
+            height: 200,
+            x: 50,
+            pos: { x: 0, y: 0 }
+        }
+    ];
+    var zoomRect = [
+        {
+            width: width/2,
+            height: (height - rectWrapHeight) / 2,
+            x: -100,
+            pos: { x: width / 2, y: 0 }
+        },
+        {
+            width: width / 2,
+            height: rectWrapHeight,
+            x: 0,
+            pos: { x: width / 2, y: (height - rectWrapHeight) / 2 }
+        },
+        {
+            width: width / 2,
+            height: (height - rectWrapHeight) / 2,
+            x: 50,
+            pos: { x: width / 2, y: (height+rectWrapHeight) / 2 }
         }
     ];
     var color = "rgba(255,238,119,1.0)";
@@ -35,35 +94,75 @@ function load()
      * @type {CanvasRenderingContext2D}
      */
     var ctx = canvas.getContext("2d");
-    var totalHeight = 0;
+    initPos(rectData);
+    initPos(nextRect);
     for (var i = 0; i < rectData.length; i++)
     {
-        rectData[i].pos.y = totalHeight;
-        totalHeight += rectData[i].wrapHeight;
+        rectTransform[i] = Object.create(rectData[i]);
+        rectTransform[i].pos = Object.create(rectData[i].pos);
     }
-    for (var i = 0; i < rectData.length; i++)
-    {
-        (function (rect)
-        {
-            var x = width / 2;
-            x += rect.x;
-            x -= rect.width / 2;
-            var y = (height - totalHeight) / 2 + rect.pos.y;
-            y += (rect.wrapHeight - rect.height) / 2;
-            rect.pos = { x: x, y: y };
-        })(rectData[i]);
-    }
+
+    setTimeout(() => {
+        targetData = zoomRect;
+        transformStartTime = time;
+        duration = 1000;
+    }, 1000);
 
     window.requestAnimationFrame(update);
     var lastDelay = 0;
+    var transformStartTime = 0;
+    var time = 0;
+    var duration = 1000;
     function update(delay)
     {
-        dt = (delay - lastDelay) / 1000;
+        time = delay;
+        var dt = (delay - lastDelay) / 1000;
+        var t = (time - transformStartTime) / duration;
+        if (t > 1)
+            t = 1;    
         ctx.fillStyle = color;
         ctx.fillRect(0, 0, width, height);
+        for (var i = 0; i < rectTransform.length; i++)
+        {
+            rectTransform[i].pos.x = interpolate(rectData[i].pos.x, targetData[i].pos.x, t);
+            rectTransform[i].pos.y = interpolate(rectData[i].pos.y, targetData[i].pos.y, t);
+            rectTransform[i].width = interpolate(rectData[i].width, targetData[i].width, t);
+            rectTransform[i].height = interpolate(rectData[i].height, targetData[i].height, t);
+
+            ctx.clearRect(rectTransform[i].pos.x, rectTransform[i].pos.y, rectTransform[i].width, rectTransform[i].height);
+        }
+        window.requestAnimationFrame(update);
+    }
+
+    function initPos(rectData)
+    {
+        var totalHeight = 0;
         for (var i = 0; i < rectData.length; i++)
         {
-            ctx.clearRect(rectData[i].pos.x, rectData[i].pos.y, rectData[i].width, rectData[i].height);
+            rectData[i].pos.y = totalHeight;
+            totalHeight += rectWrapHeight;
+        }
+        for (var i = 0; i < rectData.length; i++)
+        {
+            (function (rect)
+            {
+                var x = width / 2;
+                x += rect.x;
+                x -= rect.width / 2;
+                var y = (height - totalHeight) / 2 + rect.pos.y;
+                y += (rectWrapHeight - rect.height) / 2;
+                rect.pos = { x: x, y: y };
+            })(rectData[i]);
         }
     }
+    function interpolate(from, to, t)
+    {
+        return timingFunc(t) * (to - from) + from;
+    }
+    function timingFunc(t)
+    {
+        return t;
+    }
+
+    
 }
